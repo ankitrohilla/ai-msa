6228df2114cb729667dfd798076919840763beda
Ankit <ankit.rohillaa@yahoo.com>
2015-01-28 15:44:15 +0530

    Added files

---
+++ code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+float time;
+
+// number of characters
+int vocabNumber;
+
+// vector containing all the characters in the vocabulary
+vector<char> vocab;
+
+// number of strings
+int stringNumber;
+
+// vector of strings
+vector<string> strings;
+
+// CC*no. of dashes is the conversion cost
+int CC;
+
+
+int MC
+
+main() {
+
+
+    return 0;
+}
